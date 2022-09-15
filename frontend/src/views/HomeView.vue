1f80489934a2f06285408c410b3d1244c5cca702
Domagoj Franc <domagoj.franc@gmail.com>
2022-09-15 11:42:56 +0200

    WIP Homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -17,5 +17,125 @@
         </p>
       </div>
     </article>
+    <div class="section">
+      <div class="section__inner">
+        <h2 class="section__heading">Popular topics</h2>
+        <div class="section__categories">
+          <ul class="section__categories-list">
+            <li class="section__categories-item">
+              <RouterLink to="/" class="section__categories-link">All</RouterLink>
+            </li>
+            <li class="section__categories-item">
+              <RouterLink to="/" class="section__categories-link">Travel</RouterLink>
+            </li>
+            <li class="section__categories-item">
+              <RouterLink to="/" class="section__categories-link">Tech</RouterLink>
+            </li>
+            <li class="section__categories-item section__categories-item--last">
+              <RouterLink to="/" class="section__categories-link">View all</RouterLink>
+            </li>
+          </ul>
+        </div>
+
+        <div class="section__articles">
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
