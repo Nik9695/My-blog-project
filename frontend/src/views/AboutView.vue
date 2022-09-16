e8d160697cb8b37280c5d735939f27d7601cfa7a
Marko Janzic <janzic.marko@gmail.com>
2022-09-16 16:27:44 +0200

    Static website tweaks

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,3 +1,240 @@
 <template>
-  <h1>WE ARE ON ABOUT PAGE</h1>
+  <div>
+    <article class="mainArticle mainArticle--about">
+      <div class="mainArticle__inner mainArticle__inner--about">
+        <ul class="mainArticle__categories">
+          <li class="mainArticle__category">
+            <a href="#" class="article__category-link">Category</a>
+          </li>
+        </ul>
+        <h2 class="mainArticle__heading">
+          Richird Norton photorealistic rendering as real photos
+        </h2>
+        <p class="mainArticle__content">
+          <span class="mainArticle__content-text"
+            >Progressively incentivize cooperative systems through technically
+            sound functionalities. The credibly productivate seamless
+            data.</span
+          >
+        </p>
+        <p class="mainArticle__Author">By Jennifer Lawrence</p>
+      </div>
+    </article>
+
+    <section class="articleContent">
+      <div class="articleContent__inner">
+        <aside class="articleContent__aside">
+          <span class="articleContent__aside-text">08.08.2021.</span>
+          <span class="articleContent__aside-text">4 minutes</span>
+        </aside>
+        <div class="articleContent__content">
+          <div class="articleContent__content-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              cupiditate excepturi similique velit omnis dolorem incidunt
+              architecto quo dolore quia repudiandae ipsam est rem at aliquam,
+              quae unde necessitatibus molestias! Nam id, quas ea unde doloribus
+              aperiam distinctio asperiores mollitia rem esse temporibus
+              pariatur, harum assumenda suscipit ullam numquam corrupti.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              molestiae dolorem accusamus quis aspernatur hic consequatur,
+              possimus esse sapiente corporis deleniti veniam, tenetur rem atque
+              odit illum. Ut perferendis possimus dolor placeat nesciunt laborum
+              commodi, natus cupiditate mollitia odit pariatur asperiores
+              officia eius quasi accusantium officiis voluptatibus, distinctio
+              at magni! Ex a laboriosam hic voluptates voluptatum. Voluptatem id
+              vitae exercitationem eius eaque velit accusamus qui!
+            </p>
+            <blockquote>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam,
+              asperiores.
+            </blockquote>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              voluptatibus dignissimos dolorum laudantium mollitia dolore
+              similique ab eius ratione numquam dicta fugiat, sit libero
+              temporibus laborum sed quibusdam quae architecto qui delectus
+              commodi ipsum! Similique pariatur, hic amet tempora eveniet,
+              debitis ad consectetur recusandae doloremque, porro ipsam soluta?
+              Vel, est.
+            </p>
+          </div>
+          <ul class="articleContentTags">
+            <li class="articleContentTags__list-item">
+              <a
+                href=""
+                class="article__category-link article__category-link--tag"
+                >Category</a
+              >
+            </li>
+            <li class="articleContentTags__list-item">
+              <a
+                href=""
+                class="article__category-link article__category-link--tag"
+                >Category</a
+              >
+            </li>
+            <li class="articleContentTags__list-item">
+              <a
+                href=""
+                class="article__category-link article__category-link--tag"
+                >Category</a
+              >
+            </li>
+          </ul>
+          <div class="articleContentAuthor">
+            <div class="articleAuthor">
+              <a href="" class="articleAuthor__image-box">
+                <img
+                  class="articleAuthor__image"
+                  src="https://loremflickr.com/50/50"
+                  alt=""
+                />
+              </a>
+              <div class="articleAuthor__text">
+                <p class="articleAuthor__name">
+                  <a href="" class="articleAuthor__name-link">Name Surname</a>
+                </p>
+                <p class="articleAuthor__position">Company position</p>
+              </div>
+            </div>
+            <ul class="articleContentAuthorSocial">
+              <li class="articleContentAuthorSocial__item">
+                <a href="">
+                  <img src="https://loremflickr.com/20/20" alt="" />
+                </a>
+              </li>
+              <li class="articleContentAuthorSocial__item">
+                <a href="">
+                  <img src="https://loremflickr.com/20/20" alt="" />
+                </a>
+              </li>
+              <li class="articleContentAuthorSocial__item">
+                <a href="">
+                  <img src="https://loremflickr.com/20/20" alt="" />
+                </a>
+              </li>
+              <li class="articleContentAuthorSocial__item">
+                <a href="">
+                  <img src="https://loremflickr.com/20/20" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="relatedList relatedList--about">
+      <div class="relatedList__inner">
+        <h2 class="relatedList__heading">
+          <h3 class="relatedList__heading relatedList__heading--about">
+            Editor’s Pick
+          </h3>
+        </h2>
+        <div class="relatedList__articles">
+          <article class="article">
+            <div class="article__imagebox">
+              <RouterLink to="" class="article__imagebox-link">
+                <img
+                  src="https://loremflickr.com/420/350"
+                  alt="alt for image"
+                  class="article__image"
+                />
+              </RouterLink>
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+
+              <div class="article__content-text article__content-text--related">
+                <time class="article__content-time">08.08.2022.</time>
+                <h3 class="article__content-title">
+                  <a
+                    href=""
+                    class="article__content-title-link article__content-title-link--related"
+                    >Article title</a
+                  >
+                </h3>
+                <p class="article__content-desc">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Nobis, labore assumenda! Quaerat voluptates ullam culpa eos
+                  neque doloremque sint sapiente?
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article class="article">
+            <div class="article__imagebox">
+              <RouterLink to="" class="article__imagebox-link">
+                <img
+                  src="https://loremflickr.com/420/350"
+                  alt="alt for image"
+                  class="article__image"
+                />
+              </RouterLink>
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+
+              <div class="article__content-text article__content-text--related">
+                <time class="article__content-time">08.08.2022.</time>
+                <h3 class="article__content-title">
+                  <a
+                    href=""
+                    class="article__content-title-link article__content-title-link--related"
+                    >Article title</a
+                  >
+                </h3>
+                <p class="article__content-desc">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Nobis, labore assumenda! Quaerat voluptates ullam culpa eos
+                  neque doloremque sint sapiente?
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article class="article">
+            <div class="article__imagebox">
+              <RouterLink to="" class="article__imagebox-link">
+                <img
+                  src="https://loremflickr.com/420/350"
+                  alt="alt for image"
+                  class="article__image"
+                />
+              </RouterLink>
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+
+              <div class="article__content-text article__content-text--related">
+                <time class="article__content-time">08.08.2022.</time>
+                <h3 class="article__content-title">
+                  <a
+                    href=""
+                    class="article__content-title-link article__content-title-link--related"
+                    >Article title</a
+                  >
+                </h3>
+                <p class="article__content-desc">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Nobis, labore assumenda! Quaerat voluptates ullam culpa eos
+                  neque doloremque sint sapiente?
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
