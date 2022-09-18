f87c8339d6d0a1d0275d1e9f959e6e9f78d77a69
Nikita Agureev <nikita.agureev@internetbrands.com>
2022-09-18 12:17:06 +0300

    Adding AboutView

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,3 +1,138 @@
+<script setup></script>
+
 <template>
-  <h1>WE ARE ON ABOUT PAGE</h1>
+  <main>
+    <article class="aboutArticle">
+      <div class="aboutArticle__inner">
+        <ul class="aboutArticle__categories">
+          <li class="aboutArticle__category">
+            <a href="#" class="aboutArticle__category-link">Category</a>
+          </li>
+        </ul>
+        <h2 class="aboutArticle__heading">Richird Norton photorealistic rendering as real photos</h2>
+        <p class="aboutArticle__content">
+          <span class="aboutArticle__content-text">Progressively incentivize cooperative systems through technically sound functionalities. The credibly productivate seamless data.</span>
+        </p>
+        <p class="aboutArticle__content-author">By Jennifer Lawrence
+        </p>
+      </div>
+    </article>
+    <div class="section">
+      <div class="section__inner aboutArticle__page">
+        <div class="aboutArticle__timeline">
+          <div class="aboutArticle__date">08.08.2021</div>
+          <div class="aboutArticle__timeline-divider"></div>
+          <div class="aboutArticle__time">4 minutes</div>
+        </div>
+        <div class="aboutArticle__area">
+          <div class="aboutArticle__area-content ">
+            <div class="aboutArticle__area-excerpt">
+              <p>
+                Seamlessly syndicate cutting-edge architectures rather than collaborative collaboration and idea-sharing. Proactively incubate visionary interfaces whereas premium benefits. Seamlessly negotiate ubiquitous leadership skills rather than parallel ideas. Dramatically visualize superior interfaces for best-of-breed alignments. Synergistically formulate performance based users through customized relationships. Interactively deliver cross-platform ROI via granular systems. Intrinsicly enhance effective initiatives vis-a-vis orthogonal outsourcing. Rapidiously monetize market-driven opportunities with multifunctional users. Collaboratively enhance visionary opportunities through revolutionary schemas. Progressively network just in time customer service without real-time scenarios.
+              </p>
+              <p>
+                Synergistically drive e-business leadership with unique synergy. Compellingly seize market positioning ROI and bricks-and-clicks e-markets. Proactively myocardinate timely platforms through distributed ideas. Professionally optimize enabled core competencies for leading-edge sources. Professionally enhance stand-alone leadership with innovative synergy. Rapidiously generate backend experiences vis-a-vis long-term high-impact relationships. Authoritatively supply market-driven mindshare and bricks-and-clicks opportunities. Holisticly create diverse innovation through adaptive communities. Efficiently empower seamless meta-services with impactful opportunities. Distinctively transition virtual outsourcing with focused e-tailers.
+              </p>
+            </div>
+            <blockquote>
+              “ Monotonectally seize superior mindshare rather than efficient technology. ”
+            </blockquote>
+            <p>
+              Compellingly enhance seamless resources through competitive content. Continually actualize 24/365 alignments for resource-leveling platforms. Energistically enhance high standards in models and professional expertise. Intrinsicly iterate extensible metrics for prospective opportunities. Continually develop leading-edge experiences through quality e-services.
+            </p>
+          </div>
+          <div class="aboutArticle__area-tags">
+            <div class="aboutArticle__area-tag-item">
+              Adventure
+            </div>
+            <div class="aboutArticle__area-tag-item">
+              Photo
+            </div>
+            <div class="aboutArticle__area-tag-item">
+              Design
+            </div>
+          </div>
+          <div class="aboutArticle__area-author ">
+            <div class="aboutArticle__area-author-info ">
+              <div class="aboutArticle__area-author-photo ">
+              </div>
+              <div class="aboutArticle__area-author-content ">
+                <div class="aboutArticle__area-author-name ">By Jennifer Lawrence</div>
+                <div class="aboutArticle__area-author-title">Thinker & Designer</div>
+              </div>
+            </div>
+            <div class="aboutArticle__area-author-social-networks">
+              <a href="#" target="_blank">
+                <i class="fa-brands fa-twitter"></i>
+              </a>
+              <a href="#" target="_blank">
+                <i class="fa-brands fa-pinterest"></i>
+              </a>
+              <a href="#" target="_blank">
+                <i class="fa-brands fa-facebook"></i>
+              </a>
+              <a href="#" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="section__inner">
+        <h2 class="aboutArticle__section-heading">Related Posts</h2>
+        <div class="section__articles">
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+          <article class="article">
+            <RouterLink to="/" class="article__link">
+              <ul class="article__categories">
+                <li class="article__category">
+                  <a href="#" class="article__category-link">Category</a>
+                </li>
+              </ul>
+              <img src="https://loremflickr.com/420/350" alt="Title" class="article__image">
+              <div class="article__content">
+                <time class="article__time">10:00</time>
+                <h3 class="article__heading">Richird Norton photorealistic rendering as real photos</h3>
+                <p class="article__text">Progressively incentivize cooperative systems through technically sound
+                  functionalities. The credibly productivate seamless data.</p>
+              </div>
+            </RouterLink>
+          </article>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
