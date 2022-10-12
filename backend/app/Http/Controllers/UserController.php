96a7457a65ccc1b7a6d863ca067985578abefc0b
Nikita Agureev <nikita.agureev@internetbrands.com>
2022-10-12 08:46:45 +0300

    moving registration logic to UserController, cleaning up routes

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,14 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Comment::class, options: ['except' => ['index', 'store']]);
+    }
+
     public function index()
     {
         $users = User::paginate(5);
         return $users;
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCommentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        /*    $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'slug' => 'required',
+            'password' => 'required'
+        ]);
+        $user = new User($validated);
+        $validated['password'] = Hash::make($validated['password']); */
+
+        $user = new User($request->validated());
+        $user['password'] = Hash::make($user['password']);
+        $user->save();
+
+        return $user;
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,9 +7,12 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\UserArticleController;
 use App\Http\Controllers\UserCommentController;
 use App\Http\Controllers\UserController;
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use App\Utils\StringUtils;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rules\Password;
 
@@ -27,9 +30,6 @@ use Illuminate\Validation\Rules\Password;
 $unauthenticatedRoutes = ['index', 'show'];
 
 Route::middleware('auth:sanctum')->group(function () use ($unauthenticatedRoutes) {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
 
     Route::apiResource('articles', ArticleController::class)->except($unauthenticatedRoutes);
     Route::apiResource('comments', CommentController::class)->except($unauthenticatedRoutes);
@@ -43,35 +43,6 @@ Route::apiResource('users', UserController::class)->only($unauthenticatedRoutes)
 Route::apiResource('users.articles', UserArticleController::class)->shallow()->only($unauthenticatedRoutes);
 Route::apiResource('users.comments', UserCommentController::class)->shallow()->only($unauthenticatedRoutes);
 Route::apiResource('articles.comments', ArticleCommentController::class)->shallow()->only($unauthenticatedRoutes);
+
 Route::post('/authenticate', [AuthController::class, 'authenticate']);
-
-/* Route::post('/authenticate', function (Request $request) {
-    $credentials = $request->validate([
-        'email' => ['required', 'email'],
-        'password' => ['required', Password::min(8)]
-    ]);
-
-    if (!Auth::attempt($credentials)) {
-        return response()->json(['code' => 403, 'message' => 'invalid credentials'], 403);
-    }
-
-    $user = User::where('email', $credentials['email'])->firstOrFail();
-
-    return $user->createToken('auth_token')->plainTextToken;
-}); */
-
-/* Route::post('/registration', function (StoreUserRequest $request) {
-    $validated = $request->validate([
-        'name' => 'required',
-        'email' => 'required|email',
-        'slug' => 'required',
-        'password' => 'required'
-    ]);
-
-    $validated['password'] = Hash::make($validated['password']);
-
-    $user = new User($validated);
-    $user->save();
-
-    return $user;
-})->name('users.store'); */
+Route::post('/registration', [UserController::class, 'store']);
