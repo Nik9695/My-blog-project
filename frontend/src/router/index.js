1d555b7e6dfde86f830183c79a8d30d183c1701f
Nikita Agureev <nikita.agureev@internetbrands.com>
2022-10-16 17:28:16 +0300

    Adding authentication guard on MyProfile.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import AboutView from '../views/AboutView.vue'
 import MyProfile from '../views/MyProfile.vue'
 import Auth from '@/services/Auth'
 import { useAuthStore } from '@/store/Auth.js'
+import { useModalStore } from '@/store/Modal.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,13 +22,17 @@ const router = createRouter({
     {
       path: '/my-profile',
       name: 'my-profile',
-      component: MyProfile
+      component: MyProfile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
 
 router.beforeEach(async (to) => {
   const authStore = useAuthStore()
+  const modalStore = useModalStore()
 
   if (localStorage.getItem('token') !== null && authStore.isGuest) {
     try {
@@ -35,6 +40,13 @@ router.beforeEach(async (to) => {
       authStore.setUser(response.data)
     } catch (error) {
       authStore.logoutUser()
+    }
+  }
+
+  if (to.meta.requiresAuth && authStore.isGuest) {
+    modalStore.openModal('login')
+    return {
+      name: 'home'
     }
   }
 })
