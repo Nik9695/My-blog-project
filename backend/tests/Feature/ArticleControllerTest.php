3b981d84d6526a05268474d9578d42565f271487
Nikita Agureev <nikita.agureev@internetbrands.com>
2022-10-19 14:18:28 +0300

    Specifying 'show' method in UserArticleController. Creating test for 'show' method

--- backend/tests/Feature/ArticleControllerTest.php
+++ backend/tests/Feature/ArticleControllerTest.php
@@ -20,11 +20,25 @@ class ArticleControllerTest extends TestCase
 
     public function test_any_user_can_view_all_articles()
     {
-        $responseAll = $this->get('/api/articles');
+        $response = $this->get('/api/articles');
 
-        $responseAll->assertStatus(200);
+        $response->assertStatus(200);
 
-        $responseAll->assertJson(['per_page' => 5]);
+        $response->assertJson(['per_page' => 5]);
+    }
+
+    public function test_any_user_can_view_article_by_id()
+    {
+
+        $article = Article::factory(1)
+            ->set('title', 'Article created for testing')
+            ->set('content', 'Testing update article by owner only')
+            ->set('slug', 'testing update')
+            ->create()[0];
+
+        $response = $this->get("/api/articles/{$article->id}");
+
+        $response->assertStatus(200);
     }
 
     public function test_only_authenticated_user_can_create_article()
