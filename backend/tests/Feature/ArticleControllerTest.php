397218b6aa6adaa11c549f40424a8b82ccb5b0cb
Nikita Agureev <nikita.agureev@internetbrands.com>
2022-10-19 11:39:00 +0300

    Adding test for 'update()' method in ArticleControllerTest

--- backend/tests/Feature/ArticleControllerTest.php
+++ backend/tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Article;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -49,5 +50,49 @@ class ArticleControllerTest extends TestCase
 
         $response->assertStatus(201);
         $this->assertEquals($user->id, $response['user_id']);
+    }
+
+    public function test_only_owner_can_edit_article()
+    {
+        $owner = User::factory(1)->create()[0];
+        $user = User::factory(1)->create()[0];
+
+
+        $article = Article::factory(1)
+            ->set('title', 'Article created for testing')
+            ->set('content', 'Testing update article by owner only')
+            ->set('slug', 'testing update')
+            ->set('user_id', $owner->id)
+            ->create()[0];
+
+
+        $responseFromOwner = $this
+            ->actingAs($owner)
+            ->put(
+                "/api/articles/{$article->id}",
+                [
+                    'content' => 'Test of update method',
+                ],
+                [
+                    'Accept' => 'application/json'
+                ]
+            );
+
+        $responseFromUser = $this
+            ->actingAs($user)
+            ->put(
+                "/api/articles/{$article->id}",
+                [
+                    'content' => 'Test of update method',
+                ],
+                [
+                    'Accept' => 'application/json'
+                ]
+            );
+
+        $responseFromOwner->assertStatus(200);
+        $responseFromUser->assertStatus(403);
+
+        $this->assertEquals($owner->id, $responseFromOwner['user_id']);
     }
 }
