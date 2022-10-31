35d2149f57accb276348b1955514fb498d56d105
Nikita Agureev <nikita.agureev@internetbrands.com>
2022-10-31 13:37:06 +0300

    Adding 'vue 3 notification' library and applying it on handleError.js

--- frontend/src/helpers/handleError.js
+++ frontend/src/helpers/handleError.js
@@ -7,13 +7,9 @@ export default (error) => {
   if (error.response?.status == 422) {
     errorStore.setErrors(error.response.data.errors)
     notify({ type: 'error', text: error.response.data.message })
+    //notify({ type: 'error', text: 'Please fix all validation errors' })
   } else if (error.response?.status === 403) {
     errorStore.setErrors(error.response.data)
     notify({ type: 'error', text: error.response.data.message })
-  } else {
-    notify({
-      type: 'error',
-      text: 'Unexpected error occured, please try later.'
-    })
   }
 }
